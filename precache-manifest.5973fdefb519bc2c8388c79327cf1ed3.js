484c59afec752a8e3374f5382f97c80ed1a516cd
sybilla11 <sybilla11@users.noreply.github.com>
2020-10-24 22:32:35 -0700

    Updates

---
+++ precache-manifest.5973fdefb519bc2c8388c79327cf1ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f4ca1685f3177a4109d9a156f211a7",
+    "url": "/widget-depot/index.html"
+  },
+  {
+    "revision": "f85f3cdd20b8d462d66f",
+    "url": "/widget-depot/static/css/main.5b4b2210.chunk.css"
+  },
+  {
+    "revision": "6d47d94b7b620ff06a32",
+    "url": "/widget-depot/static/js/2.d7068441.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/widget-depot/static/js/2.d7068441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85f3cdd20b8d462d66f",
+    "url": "/widget-depot/static/js/main.4a01669a.chunk.js"
+  },
+  {
+    "revision": "b70db45becfeb645e16d",
+    "url": "/widget-depot/static/js/runtime-main.edd3ca3e.js"
+  }
+]);
